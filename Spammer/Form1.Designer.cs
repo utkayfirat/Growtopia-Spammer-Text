3b1e2994bbce4cb7209ddf0831fcbf72907e8092
Utkay Fırat <92607495+utkayfirat@users.noreply.github.com>
2022-08-22 06:15:52 +0300

    Growtopia Spammer Text
    Growtopia Text Spammer for you! (No block problem)

---
+++ Spammer/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace Spammer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startBotButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.messageTextBox = new System.Windows.Forms.RichTextBox();
+            this.intervalTextBox = new System.Windows.Forms.NumericUpDown();
+            this.processText = new System.Windows.Forms.Label();
+            this.processtextTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.intervalTextBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startBotButton
+            // 
+            this.startBotButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.startBotButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.startBotButton.FlatAppearance.BorderSize = 0;
+            this.startBotButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.startBotButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.startBotButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startBotButton.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.startBotButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.startBotButton.Location = new System.Drawing.Point(14, 230);
+            this.startBotButton.Name = "startBotButton";
+            this.startBotButton.Size = new System.Drawing.Size(356, 59);
+            this.startBotButton.TabIndex = 0;
+            this.startBotButton.Text = "Start";
+            this.startBotButton.UseVisualStyleBackColor = false;
+            this.startBotButton.Click += new System.EventHandler(this.startBotButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Message";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(14, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Milliseconds (1000 = 1)";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.messageTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.messageTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.messageTextBox.Location = new System.Drawing.Point(14, 76);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(356, 91);
+            this.messageTextBox.TabIndex = 6;
+            this.messageTextBox.Text = "";
+            // 
+            // intervalTextBox
+            // 
+            this.intervalTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.intervalTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.intervalTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.intervalTextBox.Location = new System.Drawing.Point(14, 199);
+            this.intervalTextBox.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.intervalTextBox.Name = "intervalTextBox";
+            this.intervalTextBox.Size = new System.Drawing.Size(356, 18);
+            this.intervalTextBox.TabIndex = 7;
+            // 
+            // processText
+            // 
+            this.processText.AutoSize = true;
+            this.processText.Font = new System.Drawing.Font("Arial Black", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.processText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.processText.Location = new System.Drawing.Point(12, 7);
+            this.processText.Name = "processText";
+            this.processText.Size = new System.Drawing.Size(76, 18);
+            this.processText.TabIndex = 8;
+            this.processText.Text = "[Process]";
+            // 
+            // processtextTimer
+            // 
+            this.processtextTimer.Interval = 1000;
+            this.processtextTimer.Tick += new System.EventHandler(this.processtextTimer_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(382, 45);
+            this.panel1.TabIndex = 9;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(51, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 22);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "GT-Spammer";
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label3_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(338, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 45);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Spammer.Properties.Resources.zpn;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel2.Controls.Add(this.processText);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 305);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(382, 32);
+            this.panel2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(382, 337);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.intervalTextBox);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBotButton);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Growtopia Spammer";
+            ((System.ComponentModel.ISupportInitialize)(this.intervalTextBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBotButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox messageTextBox;
+        private System.Windows.Forms.NumericUpDown intervalTextBox;
+        private System.Windows.Forms.Label processText;
+        private System.Windows.Forms.Timer processtextTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Spammer/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spammer
+{
+    public partial class Form1 : Form
+    {
+
+        bool startOrClose = true;
+        bool checkGrowtopia = false;
+
+        public const uint WM_KEYDOWN = 0x100;
+        public const uint WM_KEYUP = 0x0101;
+        public TypeConverter converter = TypeDescriptor.GetConverter(typeof(Keys));
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr Hwnd, int msg, int param, int lparam);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseCapture();
+
+
+        public Form1(){
+            InitializeComponent();
+            intervalTextBox.Value = 1000;
+            processtextTimer.Start();
+            checkGrowtopia =  growCheck();
+        }
+
+        public void Spam(IntPtr Handle, string Text)
+        {
+            PostMessage(Handle, WM_KEYDOWN, (IntPtr)(Keys.Enter), IntPtr.Zero);
+            PostMessage(Handle, WM_KEYUP, (IntPtr)(Keys.Enter), IntPtr.Zero);
+            for (int i = 0; i < Text.Length; i++)
+            {
+                Keys keys1;
+                switch (Text[i].ToString())
+                {
+                    case " ":
+                        keys1 = Keys.Space;
+                        break;
+                    case "`":
+                        keys1 = (Keys)0xC0;
+                        break;
+                    case ".":
+                        keys1 = (Keys)0x6E;
+                        break;
+                    case ",":
+                        keys1 = (Keys)0xBC;
+                        break;
+                    case "/":
+                        keys1 = (Keys)0x6F;
+                        break;
+                    case @"":
+                        keys1 = (Keys)0xDC;
+                        break;
+                    case "+":
+                        keys1 = (Keys)0x6B;
+                        break;
+                    case "-":
+                        keys1 = (Keys)0x6D;
+                        break;
+                    case ":":
+                        keys1 = (Keys)0xBA;
+                        break;
+                    case ";":
+                        keys1 = (Keys)0xBA;
+                        break;
+                    case "[":
+                        keys1 = (Keys)0xDB;
+                        break;
+                    case "]":
+                        keys1 = (Keys)0xDD;
+                        break;
+                        
+                    case "I":
+                        keys1 = (Keys)0x49;
+                        break;
+                    case "İ":
+                        keys1 = (Keys)0x49;
+                        break;
+
+                    case "|":
+                        keys1 = (Keys)0xDC;
+                        break;
+
+                    case "\t":
+                    case "\n":
+                        keys1 = Keys.Space;
+                        break;
+                    
+                    default:
+                        keys1 = (Keys)converter.ConvertFromString(Text[i].ToString().ToUpper());
+                        break;
+                }
+
+                PostMessage(Handle, WM_KEYDOWN, (IntPtr)Keys.CapsLock, IntPtr.Zero);
+                PostMessage(Handle, WM_KEYDOWN, (IntPtr)keys1, IntPtr.Zero);
+                PostMessage(Handle, WM_KEYUP, (IntPtr)keys1, IntPtr.Zero);
+            }
+            PostMessage(Handle, WM_KEYDOWN, (IntPtr)Keys.Enter, IntPtr.Zero);
+            PostMessage(Handle, WM_KEYUP, (IntPtr)Keys.Enter, IntPtr.Zero);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e){
+
+            try {
+                string windowTitle1 = "growtopia";
+                IntPtr winTitle = Process.GetProcessesByName(windowTitle1)[0].MainWindowHandle;
+                string spamText = messageTextBox.Text;
+                Spam(winTitle, spamText.ToUpper());
+            }catch(Exception ex){
+                stopSpammer();
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+
+        private void startSpammer() {
+            timer1.Start();
+            startBotButton.Text = "Close";
+            startBotButton.BackColor = Color.Thistle;
+            startBotButton.ForeColor = Color.Black;
+            startOrClose = !startOrClose;
+        }
+        private void stopSpammer() {
+            timer1.Stop();
+            startBotButton.Text = "Start";
+            startBotButton.BackColor = Color.FromArgb(23, 23, 23);
+            startBotButton.ForeColor = Color.FromArgb(1,255, 128, 255);
+            startBotButton.BackColor = Color.FromArgb(23, 23, 23);
+            startOrClose = !startOrClose;
+        }
+
+        private void startBotButton_Click(object sender, EventArgs e){
+            checkGrowtopia = growCheck();
+            if (checkGrowtopia){
+                if (intervalTextBox.Value <= 0)
+                {
+                    MessageBox.Show("Please enter interval!");
+                    return;
+                }
+
+                timer1.Interval = (int)intervalTextBox.Value;
+                if (startOrClose){
+                    startSpammer();
+                }
+                else{
+                    stopSpammer();
+                }
+                
+            }else {
+                MessageBox.Show("Please OPEN Growtopia!");
+            }
+            
+
+            
+        }
+
+        private void processtextTimer_Tick(object sender, EventArgs e){
+            checkGrowtopia = growCheck();
+        }
+
+        private bool growCheck() {
+
+            try {
+                string windowTitle1 = "growtopia";
+                IntPtr winTitle = Process.GetProcessesByName(windowTitle1)[0].MainWindowHandle;
+                processText.Text = "Growtopia ONLINE";
+                processText.ForeColor = Color.FromArgb(255, 128, 255);
+                return true;
+            }
+            catch (Exception ex) {
+                processText.Text = "Growtopia OFFLINE";
+                processText.ForeColor = Color.Purple;
+                return false;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e){
+            specialMover(e);
+        }
+
+        private void specialMover(MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+
+        private void label3_MouseDown(object sender, MouseEventArgs e)
+        {
+            specialMover(e);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            specialMover(e);
+        }
+    }
+}
